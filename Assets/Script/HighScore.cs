d47e55076bbc1f0f2170da9230091d19eff5b54c
Jeremy Wesztergom <jeremy.wesztergom@gmail.com>
2019-09-27 03:33:25 -0400

    Constructed AppleTree, Apple, and Basket with code

---
+++ Assets/Script/HighScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour
+{
+    static public int score = 1000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {                                                          
+        // If the PlayerPrefs HighScore already exists, read it
+        if (PlayerPrefs.HasKey("HighScore"))
+        {                               
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        // Assign the high score to HighScore
+        PlayerPrefs.SetInt("HighScore", score);                             
+    }
+
+    void Update()
+    {                                                        
+        Text gt = this.GetComponent<Text>();
+        gt.text = "High Score: " + score;
+        // Update the PlayerPrefs HighScore if necessary
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {                       // d
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+    }
+}
